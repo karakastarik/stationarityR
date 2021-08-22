23d752d67c2559b4b9b2c41cecf8ba65fb174c51
Ahmet Tarık Karakaş <atarikkarakas@gmail.com>
2021-08-22 23:08:12 +0300

    Initial commit

---
+++ R/summary_stationarity.R
@@ -0,0 +1,96 @@
+#' @title Aggregate KPSS, ADF and PP Test Results
+#'
+#' @description
+#' This function computes unit root test results and aggregate them in one dataframe for all possible situations.
+#'
+#' @param model fitted \code{lm(y ~ x)}  object
+#' @param lag Integer lag length. For example, if the value is \code{10}, results will come for lag lengths of \code{1:10}.
+#' @export
+#' @examples
+#' summary_stationarity(ts_model,10)
+
+summary_stationarity <- function(model,lag) {
+
+  if (lag>20) {
+    stop("Lag can not be greater than 20.")
+  }
+  kpss_type <-c("mu","tau")
+  variables <-c(as.data.frame(model$model),as.data.frame(model$residuals)) %>% as.data.frame()
+
+  #loop for kpss
+  kpss = list()
+  for (variable in names(variables)) {
+    for (type in kpss_type) {
+      for (i in 1:lag) {
+        kpss_new <- ur.kpss(variables[[variable]],type = type, use.lag=i)
+        kpss_result <- as.data.frame(cbind(Test="KPSS",Type=type,Variable=variable,Lag=i,"Statistic"=kpss_new@teststat[1],kpss_new@cval)) %>%
+          mutate(Statistic=as.numeric(.data[["Statistic"]]),
+                 "1pct"=as.numeric(.data[["1pct"]]),
+                 "5pct"=as.numeric(.data[["5pct"]]),
+                 "10pct"=as.numeric(.data[["10pct"]]))
+        row.names(kpss_result) <- NULL
+        kpss_result[["Type"]] <- ifelse(kpss_result[["Type"]]=="mu","constant","trend")
+        kpss_result["10% result"] <- ifelse(kpss_result[["Statistic"]]<kpss_result[["10pct"]],"pass","fail")
+        kpss_result["5% result"] <- ifelse(kpss_result[["Statistic"]]<kpss_result[["5pct"]],"pass","fail")
+        kpss_result["2.5% result"] <- ifelse(kpss_result[["Statistic"]]<kpss_result[["2.5pct"]],"pass","fail")
+        kpss_result["1% result"] <- ifelse(kpss_result[["Statistic"]]<kpss_result[["1pct"]],"pass","fail")
+        kpss[[paste0(i,type,variable)]] <- kpss_result # add it to list
+      }
+    }
+  }
+  kpss_full <- do.call(rbind, kpss)
+  row.names(kpss_full) <- NULL
+  #loop for adf
+  adf_type <- c("none","drift","trend")
+  adf = list()
+  for (variable in names(variables)) {
+    for (type in adf_type) {
+      for (i in 1:10) {
+        adf_new <- ur.df(variables[[variable]],type = type, lags = i)
+        adf_result <- as.data.frame(cbind(Test="ADF",Type=type,Variable=variable,Lag=i,"Statistic"=adf_new@teststat[1],as.data.frame(adf_new@cval)[1,])) %>%
+          mutate(Statistic=as.numeric(.data[["Statistic"]]),
+                 "1pct"=as.numeric(.data[["1pct"]]),
+                 "5pct"=as.numeric(.data[["5pct"]]),
+                 "10pct"=as.numeric(.data[["10pct"]]))
+        row.names(adf_result) <- NULL
+        adf_result <- adf_result %>% mutate("2.5pct"="Not avaiable for ADF") %>% relocate("2.5pct", .before = "10pct")
+        adf_result["10% result"] <- ifelse(abs(adf_result[["Statistic"]])>abs(adf_result[["10pct"]]),"pass","fail")
+        adf_result["5% result"] <- ifelse(abs(adf_result[["Statistic"]])>abs(adf_result[["5pct"]]),"pass","fail")
+        adf_result["2.5% result"] <- "Not available for ADF"
+        adf_result["1% result"] <- ifelse(abs(adf_result[["Statistic"]])>abs(adf_result[["1pct"]]),"pass","fail")
+        adf[[paste0(i,type,variable)]] <- adf_result # add it to list
+      }
+    }
+  }
+  adf_full <- do.call(rbind, adf)
+  row.names(adf_full) <- NULL
+
+  #loop for pp
+  pp_type <- c("constant","trend")
+  pp = list()
+  for (variable in names(variables)) {
+    for (type in pp_type) {
+      for (i in 1:10) {
+        pp_new <- ur.pp(variables[[variable]],type = c("Z-tau"), model=type, use.lag=i)
+        pp_result <- as.data.frame(cbind(Test="PP",Type=type,Variable=variable,Lag=i,"Statistic" = pp_new@teststat,pp_new@cval)) %>%
+          mutate(Statistic=as.numeric(.data[["Statistic"]]),
+                 "1pct"=as.numeric(.data[["1pct"]]),
+                 "5pct"=as.numeric(.data[["5pct"]]),
+                 "10pct"=as.numeric(.data[["10pct"]]))
+        row.names(pp_result) <- NULL
+        pp_result <- pp_result %>% mutate("2.5pct"="Not avaiable for PP") %>% relocate("2.5pct", .before = "10pct")
+        pp_result["10% result"] <- ifelse(abs(pp_result[["Statistic"]])>abs(pp_result[["10pct"]]),"pass","fail")
+        pp_result["5% result"] <- ifelse(abs(pp_result[["Statistic"]])>abs(pp_result[["5pct"]]),"pass","fail")
+        pp_result["2.5% result"] <- "Not available for PP"
+        pp_result["1% result"] <- ifelse(abs(pp_result[["Statistic"]])>abs(pp_result[["1pct"]]),"pass","fail")
+        pp[[paste0(i,type,variable)]] <- pp_result # add it to list
+      }
+    }
+  }
+  pp_full <- do.call(rbind, pp)
+  row.names(pp_full) <- NULL
+
+  all_tests <- rbind(kpss_full,adf_full)
+  all_tests <- rbind(all_tests,pp_full)
+  return(all_tests)
+}
---
+++ README.md
@@ -0,0 +1,30 @@
+# stationarityR
+
+## Automating Time Series Stationarity Tests
+
+This package automates Kwiatkowski–Phillips–Schmidt–Shin (KPSS), Augmented Dickey–Fuller (ADF) and Phillips–Perron (PP) unit root tests and saves the results as a dataframe.
+
+### Getting Started
+
+```devtools``` must be installed to access the ```stationarityR``` package. If not installed:
+
+```
+install.packages("devtools", dependencies = TRUE) 
+```
+Then you can use following code to install ```stationarityR``` package:
+```
+library(devtools)
+devtools::install_github("karakastarik/stationarityR")
+library(stationarityR)
+```
+### Functions
+
+For 0.1.0 release, only ```summary_stationarity(model,lag)``` function is available and this function returns a dataframe which calculates all possible combinations for unit root tests. In the function, ```model``` is fitted ```lm(y ~ x)``` object and ```lag``` is integer lag length. For example, if the ```lag``` value is 10, results will come for lag lengths of 1:10.
+
+### Output
+
+You can see the example dataframe which returned by ```summary_stationarity(model,lag)``` function below.
+
+![Output](https://github.com/karakastarik/stationarityR/blob/master/www/output.PNG)
+
+The function uses KPSS, ADF and PP unit root tests and decides whether the series is stationary or not according to the lag length, type and significance level (1%, 2.5%, 5% and 10%) as seen above. If the value is ```pass```, it means the series is stationary, and if it is ```fail```, it means the series is not stationary. You can reach this conclusion by examining statistics and critical values(10pct, 5pct...).
