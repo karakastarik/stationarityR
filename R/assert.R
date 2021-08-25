d3c469eb1cd2579c41ee0ef9f7319c5a66e7a1c3
Ahmet Tarık Karakaş <atarikkarakas@gmail.com>
2021-08-25 14:26:36 +0300

    fix

--- R/assert.R
+++ R/assert.R
@@ -1,13 +1,4 @@
-#' @title Assert the params.
-#'
-#' @description
-#' Assert.
-#'
-#' @param model fitted \code{lm(y ~ x)}  object
-#' @param lag Integer lag length. For example, if the value is \code{10}, results will come for lag lengths of \code{1:10}.
-#' @export
-#' @examples
-#' assert(ts_model,10)
+#Assert function
 assert <- function(model,lag) {
   if (lag>20 || typeof(lag) != "double") {
     stop("Lag value can not be greater than 20 and it must be integer.")
